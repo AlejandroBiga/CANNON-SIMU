cbd9cbbbe7c290ad4ab1adbc0101f8006dba136c
Alejandro Nicolas Biga <alejandro.z.biga@gmail.com>
2023-10-07 01:01:47 -0300

    Revert "b"
    This reverts commit c9f9bc7ccae5aeca7708232a3e8b7eff97baf07b.

--- Assets/scripts test/enemy IA.cs
+++ Assets/scripts test/enemy IA.cs
@@ -1,68 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.SceneManagement;
 
 public class enemyIA : MonoBehaviour
 {
-    public NavMeshAgent ai;
-    public List<Transform> destinations;
-    public Animator aiAnim;
-    public float walkSpeed, chaseSpeed, minidleTime, idleTime, maxidleTime;
-    public bool walking, chasing;
-    public Transform player;
-    Transform currentDest;
-    public int destinationAmount;
-    Vector3 dest;
-    int randNum, randNum2, randNum3;
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        walking = true;
-        randNum = Random.Range(0, destinationAmount);
-        currentDest = destinations[randNum];    
-
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        if(walking == true)
-        {
-            dest = currentDest.position;
-            ai.destination = dest;
-            ai.speed = walkSpeed;
-            if(ai.remainingDistance <= ai.stoppingDistance)
-            {
-                randNum2 = Random.Range(0, 2);
-                if(randNum2 == 0)
-                {
-                    randNum = Random.Range(0, destinationAmount);
-                    currentDest = destinations[randNum];
-                }
-                if(randNum2 == 1)
-                {
-                    ai.speed = 0;
-                    StopCoroutine("stayIdle");
-                    StartCoroutine("stayIdle");
-                    walking = false;
-                }
-            }
-
-
-
-        }
+        
     }
-
-    IEnumerator stayIdle()
-    {
-        idleTime = Random.Range(minidleTime, maxidleTime);
-        yield return new WaitForSeconds(idleTime);
-        walking = true;
-        randNum = Random.Range(0, destinationAmount);
-        currentDest = destinations[randNum];
-        aiAnim.ResetTrigger("idle");
-        aiAnim.SetTrigger("walk");
-    }
-
 }
