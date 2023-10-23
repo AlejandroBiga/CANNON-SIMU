27801803f899bab2078b6654b6a74be82d15c56b
Alejandro Nicolas Biga <alejandro.z.biga@gmail.com>
2023-10-23 16:08:45 -0300

    v2 ghost anda todo

--- Assets/ghost test/PlayerColision.cs
+++ Assets/ghost test/PlayerColision.cs
@@ -12,11 +12,18 @@ public class PlayerColision : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             ShowCanvas();
+            Destroy(other.gameObject);
+            Invoke("HideCanvas", 2f);
         }
     }
 
     private void ShowCanvas()
     {
         canvas.SetActive(true); 
+    }
+
+    private void HideCanvas()
+    {
+        canvas.SetActive(false); 
     }
 }
